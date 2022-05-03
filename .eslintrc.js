97b3b6e3c512c53ccb02251cbbfc8afdd2047a17
Dmytro Mysak <dmytro.mysak@cyberproof.com>
2022-05-03 20:46:33 +0200

    Init

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-plusplus': 0,
+    'no-continue': 0,
+    'max-len': ['error', { code: 120 }],
+  },
+  overrides: [
+    {
+      files: ["*.ts"],
+      parser: '@typescript-eslint/parser',
+      plugins: [
+        '@typescript-eslint',
+      ],
+      rules: {
+        'no-plusplus': 0,
+        'no-continue': 0,
+        'max-len': ['error', { code: 120 }],
+      },
+    }
+  ],
+};
---
+++ README.md
@@ -0,0 +1,20 @@
+# `@netly/eslint-config-base`
+
+> My personal [eslint](https://eslint.org/) config.
+
+## Usage
+
+**Install**:
+
+```bash
+$ yarn add --dev @netly/eslint-config-base
+$ npm i -D @netly/eslint-config-base
+```
+
+In eslint config file:
+
+```js
+"extends": [
+   "base"
+ ],
+```
